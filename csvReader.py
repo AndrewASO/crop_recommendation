3c8a3591b8997b41c0f07a354387e1b56a623868
AndrewASO <andrew.skevington-24@ncf.edu>
2025-07-12 22:24:00 -0400

    Added crop data csv

--- csvReader.py
+++ csvReader.py
@@ -1,9 +1,11 @@
 #This'll take in the csv. Should have some features where it'll update with new variables maybe ? 
 #Unsure of the current plan but I want to make sure its scaleable and updates whenever new variables are added
 #and the system isn't completely bricked
-#Just going to use some simple data from Google search for the test run right now
+#Just going to use some simple data from Google search for the test run right now, data isn't meant to be totally accurate and just a rough
+#approximation to test how the system & code works, along with rounding the average to the first decimal point, rainfall is measured by week and in mm
+#All of the temperatures & measurements are going to be done in metric system
 
 import pandas as pd
 
-cropData = pd.read_csv("test.csv")
+cropData = pd.read_csv("crop_data.csv")
 print(cropData.head() )
